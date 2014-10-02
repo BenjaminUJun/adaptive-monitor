8a3099c4a4eba5c9dcd5efbce678cee128140541
RichardCao <create0818@163.com>
2014-10-02 15:19:48 -0700

    add resources for dumpint

---
+++ dump/pcapdemon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import pcap
+import dpkt
+
+pc = pcap.pcap()
+pc.setfilter('tcp port 80')
+for ptime,pdata in pc:
+    print ptime,pdata
+p = dpkt.ethernet.Ethernet(pdata)
+if p.data.__class__.__name__ == 'IP':
+    ip = '%d.%d.%d.%d'%tuple(map(ord,list(p.data.dst)))
+if p.data.data.__class__.__name__ == 'TCP':
+    if p.data.data.dport == 80:
+        print p.data.data.data
