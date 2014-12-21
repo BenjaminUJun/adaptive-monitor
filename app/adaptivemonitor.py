d2f070411cd257219d1eb18759ab450531f04a0c
RichardCao <create0818@163.com>
2014-12-21 19:02:07 +0800

    add monitor flows 0.6

--- app/adaptivemonitor.py
+++ app/adaptivemonitor.py
@@ -17,7 +17,7 @@ import adaptiveswitch
 import utils
 
 
-logger.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.DEBUG,
                     format="[%(levelname)s %(asctime)s] %(name)s:: %(message)s")
 logger = logging.getLogger()
 
