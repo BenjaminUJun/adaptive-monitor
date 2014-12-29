4603ca803934e834089d1963fa748b503133eedf
RichardCao <create0818@163.com>
2014-12-29 20:45:25 +0800

    add routing based on ip 0.3

--- app/adaptivemonitor.py
+++ app/adaptivemonitor.py
@@ -152,11 +152,14 @@ class AdaptiveMonitor(adaptiveswitch.AdaptiveSwitch):
     def _request_flow_stats(datapath):
         logging.log(logging.INFO, "[INFO %s] AdaptiveMonitor._request_flow_stats datapath = %16x" % (
             time.strftime("%Y-%m-%d %H:%M:%S"), datapath.id))
+        print "aaaaaa"
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
+        print "bbbbbbb"
         req = parser.OFPFlowStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        print "ccccccc"
         datapath.send_msg(req)
+        print "ddddddd"
 
     @staticmethod
     def _request_port_stats(datapath):
